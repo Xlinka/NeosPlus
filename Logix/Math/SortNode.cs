2cb32d2e1156e641f5865e3ec2deaa222649462b
xlinka <Oliver.dunnington@outlook.com>
2022-07-24 00:49:18 +0100

    Start of NeosPlus
    Added the Sort Node from Frozen on issue https://github.com/Neos-Metaverse/NeosPublic/issues/3703
    
    And a 01I Node on the request of Ukilop to have a integer output of a bool

---
+++ Logix/Math/SortNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using BaseX;
+using FrooxEngine.UIX;
+//Credit to FroZen https://github.com/Neos-Metaverse/NeosPublic/issues/3703
+//Adds Primative Sort Node
+namespace FrooxEngine.LogiX.Operators
+{
+    [NodeName("Sort")]
+    [Category("LogiX/Math")]
+    [NodeDefaultType(typeof(Sort<float>))]
+    public class Sort<T> : LogixNode where T : IComparable
+    {
+        public readonly SyncList<Input<T>> ValueInputs;
+        public readonly Input<bool> Backward;
+        public readonly SyncList<Output<T>> ValueOutputs;
+        protected override void OnAttach()
+        {
+            base.OnAttach();
+            for (var i = 0; i < 2; i++)
+            {
+                ValueOutputs.Add();
+                ValueInputs.Add();
+            }
+        }
+        protected override void OnEvaluate()
+        {
+            var inputs = ValueInputs.Select(t => t.EvaluateRaw()).ToList();
+            inputs.Sort();
+            if (Backward.EvaluateRaw()) inputs.Reverse();
+            for (var i = 0; i < ValueOutputs.Count; i++) ValueOutputs[i].Value = inputs[i];
+        }
+        protected override void OnGenerateVisual(Slot root)
+        {
+            var uIBuilder = GenerateUI(root);
+            uIBuilder.Panel();
+            uIBuilder.HorizontalFooter(32f, out var footer, out var _);
+            var uIBuilder2 = new UIBuilder(footer);
+            uIBuilder2.HorizontalLayout(4f);
+            LocaleString text = "+";
+            var tint = color.White;
+            uIBuilder2.Button(in text, in tint, Add);
+            text = "-";
+            tint = color.White;
+            uIBuilder2.Button(in text, in tint, Remove);
+        }
+        [SyncMethod]
+        private void Add(IButton button, ButtonEventData eventData)
+        {
+            ValueInputs.Add();
+            ValueOutputs.Add();
+            RefreshLogixBox();
+        }
+        [SyncMethod]
+        private void Remove(IButton button, ButtonEventData eventData)
+        {
+            if (ValueInputs.Count <= 2) return;
+            ValueInputs.RemoveAt(ValueInputs.Count - 1);
+            ValueOutputs.RemoveAt(ValueOutputs.Count - 1);
+            RefreshLogixBox();
+        }
+    }
+}
---
+++ Logix/Operators/ZeroOneI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using FrooxEngine.LogiX.Math;
+//01I Adds a integer output of bool
+[Category(new string[] { "LogiX/Operators" })]
+[NodeName("0 1 I")]
+public class ZeroOneI : LogixOperator<int>
+{
+	public readonly Input<bool> Boolean;
+
+	public override int Content
+	{
+		get
+		{
+			if (!Boolean.EvaluateRaw(def: false))
+			{
+				return 0;
+			}
+			return 1;
+		}
+	}
+
+	public static ZeroOneI __New()
+	{
+		return new ZeroOneI();
+	}
+
+	protected override void NotifyOutputsOfChange()
+	{
+		((IOutputElement)this).NotifyChange();
+	}
+}

798ee2670d8a615a4f236564214e5f7fe706dc0f
xlinka <Oliver.dunnington@outlook.com>
2022-07-24 14:26:16 +0100

    Blank Material using PBS Shader For Testing
    Just a Blank Material with no Values for Testing purposes and to be used as a base.

---
+++ Materials/TestMaterial.cs
@@ -0,0 +1,47 @@
+using System;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using FrooxEngine.LogiX.Math;
+using BaseX;
+[Category(new string[] { "Assets/Materials/NeosPlus" })]
+public class TestMaterial : SingleShaderMaterialProvider
+{
+    protected override Uri ShaderURL => NeosAssets.Shaders.NeosPBSColorSplatSpecular; //Temp testing using the NeosPBS shader
+	public readonly Sync<float> Scale;
+	public readonly Sync<float3> Offset;
+	[DefaultValue(-1)]
+	public readonly Sync<int> RenderQueue;
+	private static PropertyState _propertyInitializationState;
+	private static MaterialProperty _Scale = new MaterialProperty("_Scale");
+	private static MaterialProperty _Offset = new MaterialProperty("_Offset");
+	public override PropertyState PropertyInitializationState
+	{
+		get
+		{
+			return _propertyInitializationState;
+		}
+		protected set
+		{
+			_propertyInitializationState = value;
+		}
+	}
+	protected override void UpdateKeywords(ShaderKeywords keywords)
+	{
+		
+	}
+	protected override void UpdateMaterial(Material material)
+    	protected override void UpdateMaterial(Material material)
+	{
+		material.UpdateFloat(_Scale, Scale);
+		material.UpdateFloat3(_Offset, Offset);
+		if (RenderQueue.GetWasChangedAndClear())
+		{
+			int renderQueue = RenderQueue.Value;
+			if ((int)RenderQueue == -1)
+			{
+				renderQueue = 2600;
+			}
+			material.SetRenderQueue(renderQueue);
+		}
+	}
+}

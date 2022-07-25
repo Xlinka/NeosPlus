92b51aca1dab9f52fedbc53b19d775449c85fde3
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-07-25 08:35:47 -0500

    Added substring

---
+++ Logix/String/CountSubstring.cs
@@ -0,0 +1,30 @@
+﻿using FrooxEngine.LogiX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FrooxEngine.Logix.String
+{
+    [Category("LogiX/String")]
+    [NodeName("Count Substring")]
+    public class CountSubstring : LogixOperator<int>
+    {
+        public readonly Input<string> String;
+        public readonly Input<string> Pattern;
+
+        public override int Content
+        {
+            get
+            {
+                if (String.EvaluateRaw() is null || Pattern.EvaluateRaw() is null)
+                    return 0;
+                else
+                    return Regex.Matches(String.EvaluateRaw(), Pattern.EvaluateRaw()).Count; // Simple but slow, might want to replace this
+            }
+        }
+        protected override void NotifyOutputsOfChange() => ((IOutputElement)this).NotifyChange();
+    }
+}

ea79d727fb97613e6de72a8b232c4c6baf343388
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-08-20 15:00:03 -0500

    Added "Is User in NoClip" LogiX node (#33)

---
+++ Logix/Locomotion/IsUserInNoClip.cs
@@ -0,0 +1,25 @@
+﻿using FrooxEngine.LogiX;
+
+namespace FrooxEngine.Logix.Locomotion
+{
+    [NodeName("Is User in NoClip")]
+    [Category(new string[] { "LogiX/Locomotion" })]
+    public class IsUserInNoClip : LogixOperator<bool>
+    {
+        public readonly Input<User> User;
+        public override bool Content
+        {
+            get
+            {
+                return User.EvaluateRaw()?.Root?.GetRegisteredComponent<LocomotionController>()?.ActiveModule.GetType() ==
+                       typeof(NoclipLocomotion);
+            }
+        }
+
+        protected override void OnCommonUpdate()
+        {
+            base.OnCommonUpdate();
+            MarkChangeDirty();
+        }
+    }
+}

b5f5591f2a19997fab76f6e564b7ec8eb5128137
frozenreflex <frozenreflex1@gmail.com>
2022-08-21 23:53:59 -0500

    Rename nodes to fit naming scheme nodes from pre-overhaul should migrate fine still, post-overhaul nodes won't but it's not released yet

--- Logix/Json/JsonGetFromObject.cs
+++ Logix/Json/JsonGetFromObject.cs
@@ -4,29 +4,28 @@ using Newtonsoft.Json.Linq;
 
 namespace FrooxEngine.LogiX.Json
 {
-    [NodeName("Quick Get From Tag")]
+    [NodeName("Get From Object")]
     [Category("LogiX/Json")]
     [GenericTypes(typeof(byte), typeof(sbyte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long),
         typeof(ulong), typeof(float), typeof(double), typeof(string), typeof(Uri), typeof(JToken), typeof(JObject),
         typeof(JArray))]
-    [OldTypeSpecialization("FrooxEngine.LogiX.Json.QuickGetNestedObject", typeof(JObject))]
-    [OldTypeSpecialization("FrooxEngine.LogiX.Json.QuickGetFromTag", typeof(string))]
-    public class JsonQuickGetFromTag<T> : LogixOperator<T>
+    [OldTypeSpecialization("FrooxEngine.LogiX.Json.GetFromTag", typeof(string))]
+    [OldTypeSpecialization("FrooxEngine.LogiX.Json.GetArrayFromTag", typeof(JArray))]
+    public class JsonGetFromObject<T> : LogixOperator<T>
     {
-        public readonly Input<string> Input;
+        public readonly Input<JObject> Input;
         public readonly Input<string> Tag;
-        protected override string Label => $"Quick Get {typeof(T).GetNiceName()} From Tag";
+        protected override string Label => $"Get {typeof(T).GetNiceName()} From Object";
         public override T Content
         {
             get
             {
                 var input = Input.EvaluateRaw();
                 var tag = Tag.EvaluateRaw();
-                if (string.IsNullOrEmpty(input) || string.IsNullOrEmpty(tag)) return default;
+                if (input == null || string.IsNullOrEmpty(tag)) return default;
                 try
                 {
-                    var input2 = JObject.Parse(input);
-                    return input2[tag].Value<T>();
+                    return input[tag].Value<T>();
                 }
                 catch
                 {

5a2526a5b078e7cc37193c1bc2b95f104d274f57
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-08-22 18:21:23 -0500

    More random

---
+++ Logix/Math/Random/RandomBool2.cs
@@ -0,0 +1,26 @@
+﻿using BaseX;
+using FrooxEngine.LogiX;
+
+namespace FrooxEngine.Logix.Math
+{
+    [Category("LogiX/Math/Random")]
+    [NodeName("Random Bool2")]
+    public class RandomBool2 : LogixNode
+    {
+        public readonly Input<float2> Chance;
+        public readonly Output<bool2> Value;
+
+        protected override void OnEvaluate()
+        {
+            var chance = Chance.EvaluateRaw(new float2(0.5f, 0.5f));
+            Value.Value = new bool2(RandomX.Chance(chance.x),
+                                    RandomX.Chance(chance.y));
+        }
+
+        protected override void OnCommonUpdate()
+        {
+            base.OnCommonUpdate();
+            MarkChangeDirty();
+        }
+    }
+}

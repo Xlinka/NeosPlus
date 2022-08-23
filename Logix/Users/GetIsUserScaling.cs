c7a2e9d513ff9a7931eec385af32710421d27521
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-08-23 11:19:26 -0500

    Scaling nodes

---
+++ Logix/Users/GetIsUserScaling.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrooxEngine.LogiX.Users
+{
+    public class GetScalingEnabled
+    {
+        [NodeName("Get User Scaling")]
+        [Category(new string[] { "LogiX/Users" })]
+        public class GetUserScaling : LogixOperator<bool>
+        {
+            public readonly Input<User> User;
+            public override bool Content
+            {
+                get
+                {
+                    var state = User.EvaluateRaw()?.Root?.GetRegisteredComponent<LocomotionController>()?.ScalingEnabled.Value;
+                    return state.HasValue ? state.Value : false;
+                }
+            }
+
+            protected override void OnCommonUpdate()
+            {
+                base.OnCommonUpdate();
+                MarkChangeDirty();
+            }
+        }
+    }
+}
---
+++ Logix/Users/SetIsUserScaling.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrooxEngine.LogiX.Users
+{
+    public class SetScalingEnabled
+    {
+        [NodeName("Set User Scaling")]
+        [Category(new string[] { "LogiX/Users" })]
+        public class SetUserScaling : LogixNode
+        {        
+            public readonly Input<User> User;
+            public readonly Input<bool> Value;
+            public readonly Impulse OnDone;
+            public readonly Impulse OnFail;
+
+            [ImpulseTarget]
+            public void Set()
+            {
+                var locomotionModule = User.EvaluateRaw()?.Root?.GetRegisteredComponent<LocomotionController>();
+                if (locomotionModule != null)
+                {
+                    locomotionModule.ScalingEnabled.Value = Value.EvaluateRaw();
+                    OnDone.Trigger();
+                }
+                else
+                {
+                    OnFail.Trigger();
+                }
+            }
+        }
+    }
+}

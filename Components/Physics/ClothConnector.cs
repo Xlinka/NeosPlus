0962908f93e2c9243612e727f452da947b088e7c
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-08-23 20:52:04 -0500

    Start of cloth sim

---
+++ Components/Physics/ClothConnector.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityNeos;
+
+namespace FrooxEngine
+{
+    public class ClothConnector : ComponentConnector<Cloth>
+    {
+        private GameObject go;
+		private UnityEngine.Cloth UnityCloth { get; set; }
+        private UnityEngine.SkinnedMeshRenderer SkinnedMeshRenderer { get; set; }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            go = new GameObject("");
+            go.transform.SetParent(attachedGameObject.transform, worldPositionStays: false);
+            go.layer = attachedGameObject.layer;
+            UnityCloth = go.AddComponent<UnityEngine.Cloth>(); // this attaches a skinned mesh renderer to the component
+            SkinnedMeshRenderer = go.GetComponent<UnityEngine.SkinnedMeshRenderer>();
+        }
+
+        public override void ApplyChanges()
+        {
+            SkinnedMeshRenderer.sharedMesh = Helper.GetUnity(Owner.Mesh.Value.Asset);
+
+            UnityCloth.stretchingStiffness = Owner.StretchingStiffness;
+            UnityCloth.bendingStiffness = Owner.BendingStiffness;
+            UnityCloth.useTethers = Owner.UseTethers;
+            UnityCloth.useGravity = Owner.UseGravity;
+            UnityCloth.damping = Owner.Damping;
+            UnityCloth.externalAcceleration = Conversions.ToUnity(Owner.ExternalAcceleration);
+            UnityCloth.randomAcceleration = Conversions.ToUnity(Owner.RandomAcceleration);
+            UnityCloth.worldVelocityScale = Owner.WorldVelocityScale;
+            UnityCloth.worldAccelerationScale = Owner.WorldAccelerationScale;
+            UnityCloth.sleepThreshold = Owner.SleepThreshold;
+            UnityCloth.enableContinuousCollision = Owner.UseContinuousCollision;
+            UnityCloth.useVirtualParticles = Owner.UseVirtualParticles;
+            UnityCloth.clothSolverFrequency = Owner.SolverFrequency;
+        }
+
+        public override void Destroy(bool destroyingWorld)
+        {
+            if (!destroyingWorld && (bool)go)
+                Object.Destroy(go);
+            go = null;
+            base.Destroy(destroyingWorld);
+        }
+    }
+}
---
+++ Components/Physics/ClothSimulation.cs
@@ -0,0 +1,68 @@
+﻿using BaseX;
+using UnityNeos;
+
+namespace FrooxEngine
+{
+    [Category(new string[] { "Rendering" })]
+    public class Cloth : ImplementableComponent, IWorker, IWorldElement
+    {
+        public readonly Sync<float> StretchingStiffness;
+        public readonly Sync<float> BendingStiffness;
+        public readonly Sync<bool> UseTethers;
+        public readonly Sync<bool> UseGravity;
+        public readonly Sync<float> Damping;
+        public readonly Sync<float3> ExternalAcceleration;
+        public readonly Sync<float3> RandomAcceleration;
+        public readonly Sync<float> WorldVelocityScale;
+        public readonly Sync<float> WorldAccelerationScale;
+        public readonly Sync<bool> UseContinuousCollision;
+        public readonly Sync<float> UseVirtualParticles;
+        public readonly Sync<float> SolverFrequency;
+        public readonly Sync<float> SleepThreshold;
+        public readonly Sync<AssetRef<Mesh>> Mesh;
+
+        // public readonly SyncList<float> CapsuleColliders;
+        // public readonly SyncList<float> SphereColliders;
+        // public readonly SyncList<float> VirtualParticleWeights;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            var grid = Slot.AttachComponent<GridMesh>();
+            var render = Slot.AttachComponent<SkinnedMeshRenderer>();
+            // render.Mesh.Asset = Mesh.Value.Asset;
+            var mat = Slot.AttachComponent<PBS_Metallic>();
+            render.Mesh.Target = grid;
+            render.Materials.Add(mat);
+            grid.Size.Value = new float2(10, 10);
+
+            Slot.WorldTransformChanged += Slot_WorldTransformChanged;
+            StretchingStiffness.Value = 1f;
+            BendingStiffness.Value = 0f;
+            UseTethers.Value = true;
+            UseGravity.Value = false;
+            Damping.Value = 0f;
+            ExternalAcceleration.Value = float3.Zero;
+            RandomAcceleration.Value = float3.Zero;
+            WorldVelocityScale.Value = 0.5f;
+            WorldAccelerationScale.Value = 1f;
+            UseContinuousCollision.Value = true;
+            UseVirtualParticles.Value = 1f;
+            SolverFrequency.Value = 120f;
+            SleepThreshold.Value = 0.1f;
+        }
+
+        protected override void OnDispose()
+        {
+            Slot.WorldTransformChanged -= Slot_WorldTransformChanged;
+            base.OnDispose();
+        }
+
+        private void Slot_WorldTransformChanged(Slot slot)
+        {
+            MarkChangeDirty();
+        }
+    }
+}
+
+

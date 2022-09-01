5d0fe7c3e1f246101d98fe93ee202d583c230549
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2022-08-31 23:39:35 -0500

    Create GetField.cs

---
+++ Logix/Components/GetField.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseX;
+
+namespace FrooxEngine.LogiX.Components
+{
+    [NodeName("Get Field")]
+    [Category("LogiX/Components")]
+    [GenericTypes(GenericTypes.Group.NeosPrimitives, typeof(RefID))]
+    public class GetField<T> : LogixOperator<IValue<T>>
+    {
+        public readonly Input<Component> Component;
+        public readonly Input<string> FieldName;
+
+        public override IValue<T> Content
+        {
+            get
+            {
+                var comp = Component.EvaluateRaw();
+                if (comp.GetSyncMember(FieldName.EvaluateRaw()) is IValue<T> field)
+                    return field;
+                return null;
+            }
+        }
+
+        protected override void NotifyOutputsOfChange() => ((IOutputElement)this).NotifyChange();
+    }
+}

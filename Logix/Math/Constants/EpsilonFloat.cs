363cca817b9f7b04688e406a7880dc70a1767a0b
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-02 08:33:43 -0500

    Update EpsilonFloat.cs

--- Logix/Math/Constants/EpsilonFloat.cs
+++ Logix/Math/Constants/EpsilonFloat.cs
@@ -3,7 +3,7 @@
 namespace FrooxEngine.LogiX.Math
 {
     [Category("LogiX/Math/Constants")]
-    [NodeName("Epsilon")]
+    [NodeName("Epsilon Float")]
     public class EpsilonFloat : LogixOperator<float>
     {
         public override float Content => MathX.FLOAT_EPSILON;

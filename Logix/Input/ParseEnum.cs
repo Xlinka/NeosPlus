5512b8f0efce0e09fabb84f92c7abe93b26e5b69
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-02 21:58:29 -0500

    Extended struct to utilize TryParse method

--- Logix/Input/ParseEnum.cs
+++ Logix/Input/ParseEnum.cs
@@ -5,7 +5,7 @@ namespace FrooxEngine
 {
     [Category(new string[] { "LogiX/Input" })]
     [GenericTypes(GenericTypes.Group.CommonEnums)]
-    public class ParseEnum<E> : LogixOperator<E> where E : Enum, IConvertible
+    public class ParseEnum<E> : LogixOperator<E> where E : struct, Enum, IConvertible
     {
         public readonly Input<string> Canidate;
         public readonly Input<bool> IgnoreCase;
@@ -16,25 +16,9 @@ namespace FrooxEngine
             get
             {
                 E testEnum;
-                IsParsed.Value = EnumTryParse(Canidate.EvaluateRaw(), IgnoreCase.EvaluateRaw(), out testEnum);
-                return testEnum;
+                IsParsed.Value = Enum.TryParse(Canidate.EvaluateRaw(), IgnoreCase.EvaluateRaw(), out testEnum);
+                return testEnum; // Logically equivalent to default(E), or the first item in an enumeration
             }
-        }
-
-        // E is not a nullable type, so we can't use Enum.TryParse. Right?
-        private static bool EnumTryParse<T>(string input, bool ignoreCase, out T theEnum)
-        {
-            StringComparison sc = ignoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture;
-            foreach (string en in Enum.GetNames(typeof(T)))
-            {
-                if (en.Equals(input, sc))
-                {
-                    theEnum = (T)Enum.Parse(typeof(T), input, ignoreCase);
-                    return true;
-                }
-            }
-            theEnum = default(T);
-            return false;
         }
     }
 }

f88fa5703053a117c8856b98718e657d53721ea1
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-07 15:12:13 -0500

    Start of SierpinskiPyramid

---
+++ Components/Assets/SierpinskiPyramid.cs
@@ -0,0 +1,35 @@
+﻿using BaseX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrooxEngine
+{
+    // https://answers.unity.com/questions/1477363/infinite-vector-3-generate-method-for-sierpinski-t.html
+    public class SierpinskiPyramid : MeshXShape
+    {
+        private readonly STetrahedron sTetrahedron = new STetrahedron();
+        public SierpinskiPyramid(MeshX mesh, int subdivisions) : base(mesh)
+        {
+            var sMesh = sTetrahedron.CreateMesh(subdivisions);
+            mesh.SetVertexCount(sMesh.VertexCapacity);
+            for (int i = 0; i < sMesh.VertexCapacity; i++)
+            {
+                mesh.SetVertex(i, sMesh.GetVertex(i).Position);
+                mesh.SetNormal(i, sMesh.GetNormal(i));
+                mesh.AddTriangle(sMesh.GetTriangle(i));
+            }
+
+            mesh.RecalculateNormals();
+            mesh.RecalculateTangents();
+        }
+
+        public override void Update()
+        {
+            Mesh.RecalculateNormals(AllTriangles);
+            Mesh.RecalculateTangents(AllTriangles);
+        }
+    }
+}
---
+++ Components/Assets/SierpinskiPyramidMesh.cs
@@ -0,0 +1,44 @@
+﻿using BaseX;
+
+namespace FrooxEngine
+{
+    [Category(new string[] { "Assets/Procedural Meshes" })]
+    public class SierpinskiPyramidMesh : ProceduralMesh
+    {
+        [Range(1, 6)]
+		public readonly Sync<int> Subdivisions;
+		private int _res;
+		private SierpinskiPyramid pyramid;
+
+		protected override void OnAwake()
+		{
+			base.OnAwake();
+			Subdivisions.Value = 1;
+			_res = 1;
+		}
+
+		protected override void ClearMeshData()
+		{
+			pyramid = null;
+		}
+
+		protected override void UpdateMeshData(MeshX meshx)
+		{
+			bool value = false;
+			if (pyramid == null)
+			{
+				pyramid?.Remove();
+				pyramid = new SierpinskiPyramid(meshx, _res);
+				value = true;
+			}
+			pyramid.Update();
+			uploadHint[MeshUploadHint.Flag.Geometry] = value;
+		}
+
+		protected override void PrepareAssetUpdateData()
+		{
+			int a = Subdivisions.Value;
+			_res = a;
+		}
+	}
+}

566e7b210aa6c8c9ab66502617cb6667193a895e
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-12 11:06:54 -0500

    staged SierpinskiPyramid

--- Components/Assets/SierpinskiPyramid.cs
+++ Components/Assets/SierpinskiPyramid.cs
@@ -13,61 +13,17 @@ namespace FrooxEngine
         public int Subdivisions;
         public SierpinskiPyramid(MeshX mesh, int subdivisions) : base(mesh)
         {
-            Subdivisions = subdivisions;
-            var sTet = new STetrahedron().SubdivideFirst(subdivisions);
-            mesh.Clear();
-            mesh.SetVertexCount(sTet.centers.Count * 12);
-
-            float s = sTet.Size;
-            int i = 0;
-            foreach (var c in sTet.centers)                                                                         
+            var sMesh = sTetrahedron.CreateMesh(subdivisions);
+            mesh.SetVertexCount(sMesh.VertexCapacity);
+            for (int i = 0; i < sMesh.VertexCapacity; i++)
             {
-                var v0 = c + new float3(0, s, 0);                                                                   
-                var v1 = c + new float3(-STetrahedron.s2_3 * s, -STetrahedron.f1_3 * s, -STetrahedron.s2_9 * s);    
-                var v2 = c + new float3(STetrahedron.s2_3 * s, -STetrahedron.f1_3 * s, -STetrahedron.s2_9 * s);
-                var v3 = c + new float3(0, -STetrahedron.f1_3 * s, STetrahedron.s8_9 * s);
-
-                var n = MathX.Cross(v2 - v0, v1 - v0).Normalized;
-                mesh.SetNormal(i, n);
-                mesh.SetNormal(i + 1, n);
-                mesh.SetNormal(i + 2, n);
-                mesh.AddTriangle(i, i + 1, i + 2);
-                mesh.SetVertex(i++, v0);
-                mesh.SetVertex(i++, v2);
-                mesh.SetVertex(i++, v1);
-
-                n = MathX.Cross(v1 - v0, v3 - v0).Normalized;
-                mesh.SetNormal(i, n);
-                mesh.SetNormal(i + 1, n);
-                mesh.SetNormal(i + 2, n);
-                mesh.AddTriangle(i, i + 1, i + 2);
-                mesh.SetVertex(i++, v0);
-                mesh.SetVertex(i++, v1);
-                mesh.SetVertex(i++, v3);
-
-                n = MathX.Cross(v3 - v0, v2 - v0).Normalized;
-                mesh.SetNormal(i, n);
-                mesh.SetNormal(i + 1, n);
-                mesh.SetNormal(i + 2, n);
-                mesh.AddTriangle(i, i + 1, i + 2);
-                mesh.SetVertex(i++, v0);
-                mesh.SetVertex(i++, v3);
-                mesh.SetVertex(i++, v2);
-
-                n = float3.Down;
-                mesh.SetNormal(i, n);
-                mesh.SetNormal(i + 1, n);
-                mesh.SetNormal(i + 2, n);
-                mesh.AddTriangle(i, i + 1, i + 2);
-                mesh.SetVertex(i++, v1);
-                mesh.SetVertex(i++, v2);
-                mesh.SetVertex(i++, v3);
+                mesh.SetVertex(i, sMesh.GetVertex(i).Position);
+                mesh.SetNormal(i, sMesh.GetNormal(i));
+                mesh.AddTriangle(sMesh.GetTriangle(i));
             }
 
-            // The following have no effects
-            // mesh.RecalculateNormals();
-            // mesh.RecalculateTangents();
-            // mesh.GetMergedDoubles();
+            mesh.RecalculateNormals();
+            mesh.RecalculateTangents();
         }
 
         public override void Update()

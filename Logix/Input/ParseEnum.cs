2052a01d1d30dfbe2270c771a158fb2c1e5aadd3
frozenreflex <frozenreflex1@gmail.com>
2022-09-22 00:31:11 -0500

    Fix includes for and cleanup ParseEnum

--- Logix/Input/ParseEnum.cs
+++ Logix/Input/ParseEnum.cs
@@ -3,7 +3,7 @@ using FrooxEngine.LogiX;
 
 namespace FrooxEngine
 {
-    [Category(new string[] { "LogiX/Input" })]
+    [Category("LogiX/Input")]
     [GenericTypes(GenericTypes.Group.CommonEnums)]
     public class ParseEnum<E> : LogixOperator<E> where E : struct, Enum, IConvertible
     {
@@ -15,8 +15,7 @@ namespace FrooxEngine
         {
             get
             {
-                E testEnum;
-                IsParsed.Value = Enum.TryParse(Canidate.EvaluateRaw(), IgnoreCase.EvaluateRaw(), out testEnum);
+                IsParsed.Value = Enum.TryParse(Canidate.EvaluateRaw(), IgnoreCase.EvaluateRaw(), out E testEnum);
                 return testEnum; // Logically equivalent to default(E), or the first item in an enumeration
             }
         }

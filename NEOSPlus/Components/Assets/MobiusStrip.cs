6d56ec8aa00281c152e2e40dcddc8a456a2e9d25
xlinka <Oliver.dunnington@outlook.com>
2023-03-16 14:17:43 +0000

    Its Morbin Time
    Mobius Strip Added

---
+++ NEOSPlus/Components/Assets/MobiusStrip.cs
@@ -0,0 +1,60 @@
+﻿using BaseX;
+using System;
+
+namespace FrooxEngine
+{
+    public class MobiusStrip : MeshXShape
+    {
+        public int Sides;
+        public int Resolution;
+        public float Width;
+
+        public MobiusStrip(MeshX mesh, int sides, int resolution, float width) : base(mesh)
+        {
+            Sides = sides;
+            Resolution = resolution;
+            Width = width;
+
+            mesh.Clear();
+            mesh.SetVertexCount(sides * resolution * 4);
+
+            int index = 0;
+            for (int side = 0; side < sides; side++)
+            {
+                for (int res = 0; res < resolution; res++)
+                {
+                    float t0 = (float)side / sides;
+                    float t1 = (float)(side + 1) / sides;
+                    float s0 = (float)res / resolution;
+                    float s1 = (float)(res + 1) / resolution;
+
+                    var v0 = GetMobiusPoint(t0, s0, width);
+                    var v1 = GetMobiusPoint(t1, s0, width);
+                    var v2 = GetMobiusPoint(t1, s1, width);
+                    var v3 = GetMobiusPoint(t0, s1, width);
+
+                    mesh.SetVertex(index, v0);
+                    mesh.SetVertex(index + 1, v1);
+                    mesh.SetVertex(index + 2, v2);
+                    mesh.SetVertex(index + 3, v3);
+
+                    mesh.AddQuadAsTriangles(index, index + 1, index + 2, index + 3);
+                    index += 4;
+                }
+            }
+        }
+
+        public override void Update()
+        {
+        }
+
+        private float3 GetMobiusPoint(float t, float s, float width)
+        {
+            float angle = 2 * MathX.PI * t;
+            float radius = 1.0f + s * width - (width / 2);
+            float3 point = new float3(MathX.Cos(angle) * radius, MathX.Sin(angle) * s * (width / 2), MathX.Sin(angle) * radius);
+
+            return point;
+        }
+    }
+}
---
+++ NEOSPlus/Components/Assets/MobiusStripMesh.cs
@@ -0,0 +1,54 @@
+﻿using BaseX;
+
+namespace FrooxEngine
+{
+    [Category(new string[] { "Assets/Procedural Meshes" })]
+    public class MobiusStripMesh : ProceduralMesh
+    {
+        [Range(3, 50)] public readonly Sync<int> Sides;
+        [Range(3, 50)] public readonly Sync<int> Resolution;
+        public readonly Sync<float> Width;
+
+        private MobiusStrip strip;
+        private int _sides;
+        private int _resolution;
+        private float _width;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            Sides.Value = 10;
+            Resolution.Value = 10;
+            Width.Value = 0.5f;
+        }
+
+        protected override void PrepareAssetUpdateData()
+        {
+            _sides = Sides.Value;
+            _resolution = Resolution.Value;
+            _width = Width.Value;
+        }
+
+        protected override void ClearMeshData()
+        {
+            strip = null;
+        }
+
+        protected override void UpdateMeshData(MeshX meshx)
+        {
+            bool value = false;
+            if (strip == null || strip.Sides != _sides || strip.Resolution != _resolution || MathX.Abs(strip.Width - _width) > MathX.FLOAT_EPSILON)
+            {
+                strip?.Remove();
+                strip = new MobiusStrip(meshx, _sides, _resolution, _width);
+                value = true;
+            }
+
+            strip.Sides = _sides;
+            strip.Resolution = _resolution;
+            strip.Width = _width;
+            strip.Update();
+            uploadHint[MeshUploadHint.Flag.Geometry] = value;
+        }
+    }
+}

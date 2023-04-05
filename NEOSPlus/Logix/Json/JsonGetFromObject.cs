cb7d70bf85bdd208caccbf19f11f8d77598ebadc
frozenreflex <frozenreflex1@gmail.com>
2023-04-05 17:42:53 -0500

    Cleanup, Remove unused generic from JsonRemoveFromArray Change JsonAddToObject method

--- NEOSPlus/Logix/Json/JsonGetFromObject.cs
+++ NEOSPlus/Logix/Json/JsonGetFromObject.cs
@@ -2,37 +2,36 @@ using System;
 using BaseX;
 using Newtonsoft.Json.Linq;
 
-namespace FrooxEngine.LogiX.Json
-{
-    [NodeName("Get From Object")]
-    [Category("LogiX/Json")]
-    [GenericTypes(typeof(byte), typeof(sbyte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long),
-        typeof(ulong), typeof(float), typeof(double), typeof(string), typeof(Uri), typeof(JToken), typeof(JObject),
-        typeof(JArray))]
-    [OldTypeSpecialization("FrooxEngine.LogiX.Json.GetFromTag", typeof(string))]
-    [OldTypeSpecialization("FrooxEngine.LogiX.Json.GetArrayFromTag", typeof(JArray))]
-    [OldTypeName("FrooxEngine.LogiX.Json.JsonGetFromTag")]
-    public class JsonGetFromObject<T> : LogixOperator<T>
-    {
-        public readonly Input<JObject> Input;
-        public readonly Input<string> Tag;
-        protected override string Label => $"Get {typeof(T).GetNiceName()} From Object";
+namespace FrooxEngine.LogiX.Json;
 
-        public override T Content
+[NodeName("Get From Object")]
+[Category("LogiX/Json")]
+[GenericTypes(typeof(byte), typeof(sbyte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long),
+    typeof(ulong), typeof(float), typeof(double), typeof(string), typeof(Uri), typeof(JToken), typeof(JObject),
+    typeof(JArray))]
+[OldTypeSpecialization("FrooxEngine.LogiX.Json.GetFromTag", typeof(string))]
+[OldTypeSpecialization("FrooxEngine.LogiX.Json.GetArrayFromTag", typeof(JArray))]
+[OldTypeName("FrooxEngine.LogiX.Json.JsonGetFromTag")]
+public class JsonGetFromObject<T> : LogixOperator<T>
+{
+    public readonly Input<JObject> Input;
+    public readonly Input<string> Tag;
+    protected override string Label => $"Get {typeof(T).GetNiceName()} From Object";
+
+    public override T Content
+    {
+        get
         {
-            get
+            var input = Input.EvaluateRaw();
+            var tag = Tag.EvaluateRaw();
+            if (input == null || string.IsNullOrEmpty(tag)) return default;
+            try
             {
-                var input = Input.EvaluateRaw();
-                var tag = Tag.EvaluateRaw();
-                if (input == null || string.IsNullOrEmpty(tag)) return default;
-                try
-                {
-                    return input[tag].Value<T>();
-                }
-                catch
-                {
-                    return default;
-                }
+                return input[tag].Value<T>();
+            }
+            catch
+            {
+                return default;
             }
         }
     }
--- NEOSPlus/Logix/Json/JsonToString.cs
+++ NEOSPlus/Logix/Json/JsonToString.cs
@@ -1,14 +1,13 @@
 using Newtonsoft.Json.Linq;
 
-namespace FrooxEngine.LogiX.Json
+namespace FrooxEngine.LogiX.Json;
+
+[NodeName("To String")]
+[Category("LogiX/Json")]
+[GenericTypes(typeof(JToken), typeof(JObject), typeof(JArray))]
+[OldTypeSpecialization("FrooxEngine.LogiX.Json.ToJSONString", typeof(JObject))]
+public class JsonToString<T> : LogixOperator<string>
 {
-    [NodeName("To String")]
-    [Category("LogiX/Json")]
-    [GenericTypes(typeof(JToken), typeof(JObject), typeof(JArray))]
-    [OldTypeSpecialization("FrooxEngine.LogiX.Json.ToJSONString", typeof(JObject))]
-    public class JsonToString<T> : LogixOperator<string>
-    {
-        public readonly Input<T> Input;
-        public override string Content => Input.EvaluateRaw()?.ToString();
-    }
+    public readonly Input<T> Input;
+    public override string Content => Input.EvaluateRaw()?.ToString();
 }

fe0d0aaed68e0d04405b2b269098fc267edfde6e
xLinka <Oliver.dunnington@outlook.com>
2023-04-07 20:56:37 +0100

    World Templates
    Templates for Creating new sessions

---
+++ NEOSPlus/World Presets/NeosPlusAssets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEOSPlus
+{
+    internal class NeosPlusAssets
+    {
+        public static Uri NeosplusGridSkybox { get; } = new Uri("neosdb:///3db0299a0f5f1d948ad94142ceecc0e5d64b2da9e0e85575d1676271f67a64ec.png");
+        public static Uri NeosplusGridFloor { get; } = new Uri("neosdb:///3db0299a0f5f1d948ad94142ceecc0e5d64b2da9e0e85575d1676271f67a64ec.png");
+    }
+}

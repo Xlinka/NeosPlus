c74a8f93765cd9bcade7f93e3311a9859243efe5
xLinka <Oliver.dunnington@outlook.com>
2023-04-12 18:22:37 +0100

    Update Crystal.cs
    awaw

--- NEOSPlus/Materials/Effects/Crystal.cs
+++ NEOSPlus/Materials/Effects/Crystal.cs
@@ -26,23 +26,23 @@ public class CrystalMaterial : SingleShaderMaterialProvider
     public readonly Sync<int> LightCompletion;
     public readonly Sync<float> Cutoff;
 
-    private static MaterialProperty _BaseColorTextureProp = new MaterialProperty("_BaseColorTexture");
-    private static MaterialProperty _BaseColorProp = new MaterialProperty("_BaseColor");
-    private static MaterialProperty _SurfaceColorTextureProp = new MaterialProperty("_SurfaceColorTexture");
-    private static MaterialProperty _SurfaceColorProp = new MaterialProperty("_SurfaceColor");
-    private static MaterialProperty _NormalTextureProp = new MaterialProperty("_NormalTexture");
-    private static MaterialProperty _AlphaTextureProp = new MaterialProperty("_AlphaTexture");
-    private static MaterialProperty _MetallicProp = new MaterialProperty("_Metallic");
+    private static MaterialProperty _BaseColortex = new MaterialProperty("_BaseColortex");
+    private static MaterialProperty _BasaColor = new MaterialProperty("_BasaColor");
+    private static MaterialProperty _SurfaceClolrtex = new MaterialProperty("_SurfaceClolrtex");
+    private static MaterialProperty _SurfaceClolr = new MaterialProperty("SurfaceClolr");
+    private static MaterialProperty _Normal = new MaterialProperty("_Normal");
+    private static MaterialProperty _alpha = new MaterialProperty("_alpha");
+    private static MaterialProperty _Metallic = new MaterialProperty("_Metallic");
     private static MaterialProperty _GlossProp = new MaterialProperty("_Gloss");
-    private static MaterialProperty _RepetitionProp = new MaterialProperty("_Repetition");
-    private static MaterialProperty _ColorLoopProp = new MaterialProperty("_ColorLoop");
+    private static MaterialProperty _Repetition = new MaterialProperty("_Repetition");
+    private static MaterialProperty _ColorLoop = new MaterialProperty("_ColorLoop");
     private static MaterialProperty _WidthProp = new MaterialProperty("_Width");
-    private static MaterialProperty _ColorLevelProp = new MaterialProperty("_ColorLevel");
-    private static MaterialProperty _PastelColorProp = new MaterialProperty("_PastelColor");
-    private static MaterialProperty _DistortionProp = new MaterialProperty("_Distortion");
-    private static MaterialProperty _ChromaticAberrationProp = new MaterialProperty("_ChromaticAberration");
-    private static MaterialProperty _LightCompletionProp = new MaterialProperty("_LightCompletion");
-    private static MaterialProperty _CutoffProp = new MaterialProperty("_Cutoff");
+    private static MaterialProperty _ColoeLevel = new MaterialProperty("_ColoeLevel");
+    private static MaterialProperty _PasutelColor = new MaterialProperty("_PasutelColor");
+    private static MaterialProperty _Distortion = new MaterialProperty("_Distortion");
+    private static MaterialProperty _ChromaticAberration = new MaterialProperty("_ChromaticAberration");
+    private static MaterialProperty _LightCompletion = new MaterialProperty("_LightCompletion");
+    private static MaterialProperty _Cutoff = new MaterialProperty("_Cutoff");
 
     [DefaultValue(-1)]
     public readonly Sync<int> RenderQueue;
@@ -63,23 +63,23 @@ public class CrystalMaterial : SingleShaderMaterialProvider
 
     protected override void UpdateMaterial(Material material)
     {
-        material.UpdateTexture(_BaseColorTextureProp, BaseColorTexture);
-        material.UpdateColor(_BaseColorProp, BaseColor);
-        material.UpdateTexture(_SurfaceColorTextureProp, SurfaceColorTexture);
-        material.UpdateColor(_SurfaceColorProp, SurfaceColor);
-        material.UpdateTexture(_NormalTextureProp, NormalTexture);
-        material.UpdateTexture(_AlphaTextureProp, AlphaTexture);
-        material.UpdateFloat(_MetallicProp, Metallic);
+        material.UpdateTexture(_BaseColortex, BaseColorTexture);
+        material.UpdateColor(_BasaColor, BaseColor);
+        material.UpdateTexture(_SurfaceClolrtex, SurfaceColorTexture);
+        material.UpdateColor(_SurfaceClolr, SurfaceColor);
+        material.UpdateTexture(_Normal, NormalTexture);
+        material.UpdateTexture(_alpha, AlphaTexture);
+        material.UpdateFloat(_Metallic, Metallic);
         material.UpdateFloat(_GlossProp, Gloss);
-        material.UpdateFloat(_RepetitionProp, Repetition);
-        material.UpdateFloat(_ColorLoopProp, ColorLoop);
+        material.UpdateFloat(_Repetition, Repetition);
+        material.UpdateFloat(_ColorLoop, ColorLoop);
         material.UpdateFloat(_WidthProp, Width);
-        material.UpdateFloat(_ColorLevelProp, ColorLevel);
-        material.UpdateFloat(_PastelColorProp, PastelColor);
-        material.UpdateFloat(_DistortionProp, Distortion);
-        material.UpdateFloat(_ChromaticAberrationProp, ChromaticAberration);
-        material.UpdateInt(_LightCompletionProp, LightCompletion);
-        material.UpdateFloat(_CutoffProp, Cutoff);
+        material.UpdateFloat(_ColoeLevel, ColorLevel);
+        material.UpdateFloat(_PasutelColor, PastelColor);
+        material.UpdateFloat(_Distortion, Distortion);
+        material.UpdateFloat(_ChromaticAberration, ChromaticAberration);
+        material.UpdateInt(_LightCompletion, LightCompletion);
+        material.UpdateFloat(_Cutoff, Cutoff);
     }
 }
 

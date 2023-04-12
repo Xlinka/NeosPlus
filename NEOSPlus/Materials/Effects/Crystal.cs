45706bb7a2802abb910738b59dec5ad4951509db
xLinka <Oliver.dunnington@outlook.com>
2023-04-12 19:27:17 +0100

    Updated Crystal.cs to have defaults.
    defaults added

--- NEOSPlus/Materials/Effects/Crystal.cs
+++ NEOSPlus/Materials/Effects/Crystal.cs
@@ -14,17 +14,30 @@ public class CrystalMaterial : SingleShaderMaterialProvider
     public readonly Sync<color> SurfaceColor;
     public readonly AssetRef<ITexture2D> NormalTexture;
     public readonly AssetRef<ITexture2D> AlphaTexture;
+
+    [Range(0f, 1f, "0.00")]
     public readonly Sync<float> Metallic;
+    [Range(0f, 1f, "0.00")]
     public readonly Sync<float> Gloss;
+    [Range(1f, 20f, "0.00")]
     public readonly Sync<float> Repetition;
+    [Range(1f, 5f, "0.00")]
     public readonly Sync<float> ColorLoop;
+    [Range(0f, 1f, "0.00")]
     public readonly Sync<float> Width;
+    [Range(0f, 1f, "0.00")]
     public readonly Sync<float> ColorLevel;
+    [Range(0f, 1f, "0.00")]
     public readonly Sync<float> PastelColor;
+    [Range(0f, 10f, "0.00")]
     public readonly Sync<float> Distortion;
+    [Range(-1f, 1f, "0.00")]
     public readonly Sync<float> ChromaticAberration;
+    [Range(0f, 1f, "0.00")]
     public readonly Sync<int> LightCompletion;
-    public readonly Sync<float> Cutoff;
+    [Range(0f, 1f, "0.00")]
+    // needs to be a bool but how the hell do i do a material update bool i need to find a example of this.
+    public readonly Sync<float> Cutoff; //supposed to be hidden but eh
 
     private static MaterialProperty _BaseColortex = new MaterialProperty("_BaseColortex");
     private static MaterialProperty _BasaColor = new MaterialProperty("_BasaColor");
@@ -58,7 +71,17 @@ public class CrystalMaterial : SingleShaderMaterialProvider
     protected override void OnAttach()
     {
         base.OnAttach();
-        //add default values when i wakeup - xlinka
+        Metallic.Value = 0.5f;
+        Gloss.Value = 0.9f;
+        Repetition.Value = 10f;
+        ColorLoop.Value = 1f;
+        Width.Value = 0.5f;
+        ColorLevel.Value = 0.5f;
+        PastelColor.Value = 0.3f;
+        Distortion.Value = 5f;
+        ChromaticAberration.Value = 0f;
+        LightCompletion.Value = 1;
+        Cutoff.Value = 0.5f;
     }
 
     protected override void UpdateMaterial(Material material)
@@ -80,6 +103,11 @@ public class CrystalMaterial : SingleShaderMaterialProvider
         material.UpdateFloat(_ChromaticAberration, ChromaticAberration);
         material.UpdateInt(_LightCompletion, LightCompletion);
         material.UpdateFloat(_Cutoff, Cutoff);
+
+        if (!RenderQueue.GetWasChangedAndClear()) return;
+        var renderQueue = RenderQueue.Value;
+        if ((int)RenderQueue == -1) renderQueue = 3000;
+        material.SetRenderQueue(renderQueue);
     }
 }
 

efe1710f04fa1cf6359d3478477d50852e6a1208
xLinka <Oliver.dunnington@outlook.com>
2023-04-17 15:05:43 +0100

    Write To Log Logix Node
    Write to Log Node For Debugging Stuff to write it to the log of neosvr using the unilog from basex

---
+++ NEOSPlus/Logix/Utility/Write To Log.cs
@@ -0,0 +1,39 @@
+﻿using BaseX;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+
+namespace FrooxEngine.LogiX.Utility
+{
+    public enum LogSeverity
+    {
+        Log,
+        Warning,
+        Error
+    }
+
+    [Category("LogiX/Utility")]
+    [NodeName("Write to Log")]
+    public class WriteToLog<T> : LogixNode
+    {
+        public readonly Input<T> Value;
+        public readonly Input<LogSeverity> Severity;
+        public readonly Input<string> Tag;
+
+        [ImpulseTarget]
+        public void Write()
+        {
+            switch (Severity.Evaluate())
+            {
+                case LogSeverity.Log:
+                    UniLog.Log(Tag.EvaluateRaw() + Value.EvaluateRaw()?.ToString());
+                    break;
+                case LogSeverity.Warning:
+                    UniLog.Warning(Tag.EvaluateRaw() + Value.EvaluateRaw()?.ToString());
+                    break;
+                case LogSeverity.Error:
+                    UniLog.Error(Tag.EvaluateRaw() + Value.EvaluateRaw()?.ToString());
+                    break;
+            }
+        }
+    }
+}

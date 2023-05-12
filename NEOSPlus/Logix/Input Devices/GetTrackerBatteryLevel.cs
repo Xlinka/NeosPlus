130dfbab5d5ea2740ecaa197798d043a64513334
art0007i <art0007i@gmail.com>
2023-05-12 18:59:56 +0200

    it wasn't working correctly before

--- NEOSPlus/Logix/Input Devices/GetTrackerBatteryLevel.cs
+++ NEOSPlus/Logix/Input Devices/GetTrackerBatteryLevel.cs
@@ -10,23 +10,43 @@ using System.Collections.Generic;
 public class GetTrackerBatteryLevel : LogixNode
 {
     public readonly Input<User> User;
-    public readonly Input<BodyNode> TrackerIndex;
+    public readonly Input<BodyNode> BodyNode;
     public readonly Output<float> BatteryLevel;
+    public readonly Output<bool> IsBatteryCharging;
+
+    private ViveTracker _lastTracker;
+    protected readonly SyncRef<ValueStream<float>> _batteryLevelStream;
+    protected readonly SyncRef<ValueStream<bool>> _batteryChargingStream;
+
+    protected override void OnCommonUpdate()
+    {
+        base.OnCommonUpdate();
+        User user = User.Evaluate();
+        var bodyNode = BodyNode.Evaluate();
+        if (user == base.LocalUser)
+        {
+            ViveTracker device = InputInterface.GetDevices<ViveTracker>().Find((t) => t.CorrespondingBodyNode == bodyNode);
+            if (device != _lastTracker)
+            {
+                _batteryLevelStream.Target = device?.BatteryLevel.GetStream(base.World);
+                _batteryChargingStream.Target = device?.BatteryCharging.GetStream(base.World);
+                _lastTracker = device;
+            }
+        }
+        else
+        {
+            _lastTracker = null;
+            if (base.World.IsAuthority && user == null)
+            {
+                _batteryLevelStream.Target = null;
+                _batteryChargingStream.Target = null;
+            }
+        }
+    }
 
     protected override void OnEvaluate()
     {
-        User user = User.Evaluate();
-        if (user != null)
-        {
-            var trackerIndex = TrackerIndex.Evaluate();
-            ViveTracker tracker = user.InputInterface.GetDevices<ViveTracker>().Find((t) => t.CorrespondingBodyNode == trackerIndex);
-            if (tracker != null)
-            {
-                BatteryLevel.Value = tracker.BatteryLevel.Value;
-                return;
-            }
-        }
-
-        BatteryLevel.Value = 0f;
+        BatteryLevel.Value = _batteryLevelStream.Target?.Value ?? (-1f);
+        IsBatteryCharging.Value = _batteryChargingStream.Target?.Value ?? false;
     }
 }

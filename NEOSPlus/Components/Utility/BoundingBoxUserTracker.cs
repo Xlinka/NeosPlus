4b117f90c974a7718e476e21c66430b1ac1861a5
Sam Scott <samueltscott@gmail.com>
2023-07-22 21:03:23 +0100

    Add BoundingBoxUserTracker.cs

---
+++ NEOSPlus/Components/Utility/BoundingBoxUserTracker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BaseX;
+
+namespace FrooxEngine
+{
+	[Category("Utility")]
+	public class BoundingBoxUserTracker : Component
+	{
+		public readonly RawOutput<bool> IsLocalUserInside;
+
+		public readonly RawOutput<bool> IsAnyUserInside;
+
+		public readonly RawOutput<int> NumberOfUsersInside;
+
+		protected readonly SyncBag<UserRef> _usersInside;
+
+		private List<User> usersInWorld = new List<User>();
+
+		protected override void OnChanges()
+		{
+			base.OnChanges();
+			_usersInside.RemoveAll((UserRef u) => u.Target == null);
+			IsAnyUserInside.Value = _usersInside.Count > 0;
+			NumberOfUsersInside.Value = _usersInside.Count;
+			IsLocalUserInside.Value = _usersInside.Any((KeyValuePair<RefID, UserRef> u) => u.Value.Target == this.LocalUser);
+		}
+
+		protected override void OnCommonUpdate()
+		{
+			base.OnCommonUpdate();
+			usersInWorld.Clear();
+			this.World.GetUsers(usersInWorld);
+			BoundingBox b = this.Slot.ComputeBoundingBox();
+			foreach (User user in usersInWorld)
+			{
+				if (MathX.IsBetween(user.Root.Slot.GlobalPosition, b.min, b.max))
+				{
+					if (!_usersInside.Any((KeyValuePair<RefID, UserRef> u) => u.Value.Target == user))
+					{
+						_usersInside.Add().Target = user;
+					}
+				}
+				else
+				{
+					if (_usersInside.Any((KeyValuePair<RefID, UserRef> u) => u.Value.Target == user))
+					{
+						_usersInside.RemoveAll((UserRef u) => u.Target == user);
+					}
+				}
+			}
+		}
+	}
+}

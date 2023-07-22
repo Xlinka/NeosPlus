aca6416c71b2d76319b786a29d514152a1d507d7
Sam Scott <samueltscott@gmail.com>
2023-07-22 12:50:17 +0100

    Add ExtractIDs node

---
+++ NEOSPlus/Logix/References/ExtractIDs.cs
@@ -0,0 +1,27 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+using BaseX;
+
+[Category(new string[] { "LogiX/References" })]
+public class ExtractIDs : LogixNode
+{
+    public readonly Input<RefID> RefID;
+
+    public readonly Output<ulong> Position;
+
+    public readonly Output<byte> User;
+
+    protected override void OnEvaluate()
+    {
+        RefID refId = RefID.EvaluateRaw();
+        if (refId == null)
+        {
+            Position.Value = 0;
+            User.Value = 0;
+            return;
+        }
+        refId.ExtractIDs(out var position, out var user);
+        Position.Value = position;
+        User.Value = user;
+    }
+}

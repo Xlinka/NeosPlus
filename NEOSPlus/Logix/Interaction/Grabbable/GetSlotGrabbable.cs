336c7d858e960523f20442039e79aa8a7327407b
Sam Scott <samueltscott@gmail.com>
2023-07-22 11:49:30 +0100

    Optimized the node maybe?

---
+++ NEOSPlus/Logix/Interaction/Grabbable/GetSlotGrabbable.cs
@@ -0,0 +1,24 @@
+﻿namespace FrooxEngine.LogiX.Interaction
+{
+	[Category("LogiX/Interaction/Grabbable")]
+	[NodeName("Get Slot Grabbable")]
+	public class GetSlotGrabbable : LogixNode
+	{
+		public readonly Input<Slot> Instance;
+
+		public readonly Output<IGrabbable> Grabbable;
+
+		protected override void OnEvaluate()
+		{
+			Slot slot = Instance.EvaluateRaw();
+			if (slot == null)
+			{
+				Grabbable.Value = null;
+			}
+			else
+			{
+				Grabbable.Value = slot.GetComponent<Grabbable>();
+			}
+		}
+	}
+}

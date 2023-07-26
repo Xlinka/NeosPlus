4eba9e3b330fa9b02c7fc4f0b0f97f0da3afe19b
Sam Scott <samueltscott@gmail.com>
2023-07-26 16:24:09 +0100

    Add Get Children With Tag node

---
+++ NEOSPlus/Logix/WorldModel/GetChildrenWithTag.cs
@@ -0,0 +1,28 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+using System.Collections.Generic;
+
+[Category("LogiX/Slots")]
+[NodeName("Get Children With Tag")]
+public class GetChildrenWithTag : LogixNode
+{
+    public readonly Input<Slot> Instance;
+
+    public readonly Input<string> Tag;
+
+    public readonly Output<List<Slot>> FoundChildren;
+
+    protected override void OnEvaluate()
+    {
+        Slot slot = Instance.Evaluate();
+        if (slot != null)
+        {
+            string tag = Tag.Evaluate();
+            FoundChildren.Value = slot.GetChildrenWithTag(tag);
+        }
+        else
+        {
+            FoundChildren.Value = null;
+        }
+    }
+}

581b2606e00be30a87234e07d0de497e61b8e52a
Nytra <samueltscott@gmail.com>
2023-07-27 17:46:10 +0100

    Add GetAllChildren and GetAllParents nodes

---
+++ NEOSPlus/Logix/Slots/GetAllChildren.cs
@@ -0,0 +1,25 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+using System.Collections.Generic;
+
+[Category("LogiX/Slots")]
+[NodeName("Get All Children")]
+public class GetAllChildren : LogixNode
+{
+    public readonly Input<Slot> Instance;
+
+    public readonly Output<List<Slot>> FoundChildren;
+
+    protected override void OnEvaluate()
+    {
+        Slot slot = Instance.Evaluate();
+        if (slot != null)
+        {
+            FoundChildren.Value = slot.GetAllChildren();
+        }
+        else
+        {
+            FoundChildren.Value = null;
+        }
+    }
+}
---
+++ NEOSPlus/Logix/Slots/GetAllParents.cs
@@ -0,0 +1,27 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+using System.Collections.Generic;
+
+[Category("LogiX/Slots")]
+[NodeName("Get All Parents")]
+public class GetAllParents : LogixNode
+{
+    public readonly Input<Slot> Instance;
+
+    public readonly Output<List<Slot>> FoundParents;
+
+    protected override void OnEvaluate()
+    {
+        Slot slot = Instance.Evaluate();
+        if (slot != null)
+        {
+            List<Slot> list = new List<Slot>();
+            slot.GetAllParents(list);
+            FoundParents.Value = list;
+        }
+        else
+        {
+            FoundParents.Value = null;
+        }
+    }
+}
